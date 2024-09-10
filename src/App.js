92b9c9f1dd49d4e433b13494353c22a588f12281
shakil-ahmmed-se <mdshakilahmmed8906@gmail.com>
2024-09-11 02:37:02 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+import { useState } from 'react';
 
 function App() {
+
+  const [likeColor, setLikeColor] = useState('');
+
+  const handleLikeClick = () => {
+      const color = likeColor ? '' : 'primary';
+      setLikeColor(color);
+  }
+
   return (
     <div className="App">
+      <AccessAlarmIcon />
+      <ThumbUpIcon onClick={handleLikeClick} color={likeColor} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
